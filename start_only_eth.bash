df13a614a4f6b1b287e16425bd5f1eb3016eca5d
avenger <pravinjothinathan@gmail.com>
2017-10-03 23:31:27 -0700

    Adding files for reference

---
+++ start_only_eth.bash
@@ -0,0 +1,7 @@
+export GPU_FORCE_64BIT_PTR=0
+export GPU_MAX_HEAP_SIZE=100
+export GPU_USE_SYNC_OBJECTS=1
+export GPU_MAX_ALLOC_PERCENT=100
+export GPU_SINGLE_ALLOC_PERCENT=100
+
+./ethdcrminer64 -epool eth-us-west1.nanopool.org:9999 -ewal 0xa90221e1ee66582dfd1bafc7c534e9e7ed6ab7ea/avenger/pravinpuma@gmail.com -epsw x -mode 1 -ftime 10
